28b17a2d0cde23c4ebafc18cea3b2da34d8ca8b4
MargaritaYatina <pearlmargaret2012@gmail.com>
2022-04-02 00:07:25 -0700

    t

--- DataStructure Tutorial Practise/LinkedList/push.js
+++ DataStructure Tutorial Practise/LinkedList/push.js
@@ -1,22 +1,69 @@
 //implementation of Node class
 class Node {
-    constractor(val){
-        this.val = val;
-        this.next =null;
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+    }
+ }
+class SinglyLinkedList {
+    constructor() {
+      this.length = 0;
+      this.head = null;
+      this.tail = null;
+    }
+    push(val) {
+      const newNode = new Node(val)
+      if (this.head===null) { // happens only once
+        this.head = newNode;
+        this.tail = this.head;
+      } else {
+        this.tail.next = newNode;   // this.a.next = b node???
+        this.tail = newNode;
+      }
+      this.length++;
+    }
+
+    traverse(){
+        let current = this.head;
+        while(current){
+            console.log(current.val);
+            current=current.next;
+        }
+    }
+
+    pop(){
+        if(list.length===0){
+            return undefined;
+        }
+        else{
+            let current = this.head;
+            let newTail =current;
+            while(current.next){
+                newTail = current;
+                current= current.next;
+                
+            }
+            this.tail=newTail;
+            this.tail.next = null;
+            this.length --;
+            if(this.length===0){
+                this.head=null;
+                this.tail=null;
+            }
+        }
+        return this;
     }
 }
-
-class LinkedList {
-    constractor(){
-        this.head =null;
-        this.tail =null;
-        this.length = 0;
-    }
-}
-
 
 //test case
-let newNode = new Node("hi");
-newNode.next = new Node ("you");
+let list = new SinglyLinkedList();
+list.push("first");
 
-console.log(newNode);
+console.log(list.pop());
+console.log(list.pop());
+console.log(list);
+
+
+
+
+
